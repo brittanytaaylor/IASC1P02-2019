ff5580afcfb08de336a1acf64194ed60f5c553a3
Brittany Taylor <bt15dn@brocku.ca>
2019-11-08 23:49:07 -0500

    Lab 8

---
+++ Lab 8/scripts.js
@@ -0,0 +1,33 @@
+document.write("<p> I love Monkeys </p>");
+
+document.write("Hello World");
+
+function changeText(1){
+
+  document.getElementById("one").style.color = "blue";
+  document.getElementById("one").style.fontSize = "20px";
+
+  document.getElementById("two").style.color = "green";
+  document.getElementById("two").style.fontSize = "5px";
+
+  document.getElementById("three").style.color = "orange";
+  document.getElementById("three").style.fontSize = "10px";
+
+
+}
+
+function changeText(2) {
+  4
+}
+
+function changeText(3) {
+  0
+}
+
+function changeText(4) {
+  4
+}
+
+function changeText(5) {
+  1
+}
