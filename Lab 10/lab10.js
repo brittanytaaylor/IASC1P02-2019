cb01d6500a17ac5b2884745775822b88911d18d1
Brittany Taylor <bt15dn@brocku.ca>
2019-11-22 22:28:56 -0500

    Lab 10

---
+++ Lab 10/lab10.js
@@ -0,0 +1,20 @@
+var number1="";
+
+var number2="";
+
+var number3=number1 + number2;
+
+var number4=number1 - number2;
+
+var number5=number1 * number2;
+
+var number6=number1 / number2;
+
+document.write (number3);
+document.write (number4);
+document.write (number5);
+document.write (number6);
+
+function changeText (){
+
+}
